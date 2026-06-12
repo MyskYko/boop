21246790a02580f309a422981843f1bd2a6e3dec
MyskYko <yoyuohlhjl@yahoo.co.jp>
2026-06-12 20:43:00 +0200

    add a top-level test

---
+++ test/run_helo.cmake
@@ -0,0 +1,25 @@
+get_filename_component(OUTPUT_DIR "${OUTPUT_PATH}" DIRECTORY)
+file(MAKE_DIRECTORY "${OUTPUT_DIR}")
+
+execute_process(
+  COMMAND "${HELO_EXECUTABLE}" "${INPUT_PATH}" "${OUTPUT_PATH}" --timeout 1
+  RESULT_VARIABLE HELO_RESULT
+)
+if(NOT HELO_RESULT EQUAL 0)
+  message(FATAL_ERROR "${TEST_NAME}: helo failed with exit code ${HELO_RESULT}")
+endif()
+
+if(DEFINED ABC_EXECUTABLE)
+  execute_process(
+    COMMAND "${ABC_EXECUTABLE}" -c "cec -n ${INPUT_PATH} ${OUTPUT_PATH}"
+    RESULT_VARIABLE CEC_RESULT
+    OUTPUT_VARIABLE CEC_OUTPUT
+    ERROR_VARIABLE CEC_ERROR
+  )
+  if(NOT CEC_RESULT EQUAL 0)
+    message(FATAL_ERROR "${TEST_NAME}: ABC cec failed with exit code ${CEC_RESULT}\n${CEC_OUTPUT}\n${CEC_ERROR}")
+  endif()
+  if(NOT CEC_OUTPUT MATCHES "Networks are equivalent\\.")
+    message(FATAL_ERROR "${TEST_NAME}: ABC cec did not prove equivalence\n${CEC_OUTPUT}\n${CEC_ERROR}")
+  endif()
+endif()
